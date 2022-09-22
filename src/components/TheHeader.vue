1d4de686d4fd6fa61ffe5950190642075f631ea0
unknown <omiddellcash@gmail.com>
2022-09-22 13:46:50 +0330

    working on header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -8,26 +8,41 @@ const imgs = {
 </script>
 
 <template>
-    <header bg-white p3 >
+    <header bg-white p3 md:p5 xl:py3>
         <section layout divide-y divide-stone-100 space-y2>
             <div flex items-center justify-between>
-                <div flex items-center space-x-reverse space-x10>
-                    <div :class="imgs.hamburger" text-2xl />
+                <div flex items-center space-x-reverse space-x10 xl:space-x0>
+                    <div :class="imgs.hamburger" text-2xl md:text-3xl xl:hidden />
                     <div flex items-center>
-                        <img :src="imgs.logo" alt="logo" w8 h8>
-                        <h3 class="text-xs -tracking-1px mr3">نام فروشگاه شما</h3>
+                        <img :src="imgs.logo" alt="logo" w8 h8 md="w10 h10">
+                        <h3 class="text-xs -tracking-1px mr3 md:text-lg">نام فروشگاه شما</h3>
                     </div>
                 </div>
-                <div :class="imgs.basket" text-2xl text-stone-500 />
+
+                <input type="text" placeholder="نام کالا٬ برند یا دسته‌بندی مورد نظر . . ."
+                    class="hidden md:block text-xs placeholder:text-2.5 px3 py2.7 bg-stone-200/70 rounded-lg duration-250 focus:outline-none focus:outline focus:outline-stone-400 md:placeholder:text-3 w-65 xl:px5 xl:py3.83.83.8 xl:w-130 xl:-mr30">
+
+                <div
+                    class="hidden md:flex items-center border border-stone-100 w-35 px-3 py1.5 rounded-lg xl:py2.5 xl:px-5 xl:w40 xl:-ml40">
+                    <div :class="imgs.user" text-stone-500 xl:text-xl />
+
+                    <div
+                        class="[&_span]:text-10px [&_span]:text-stone-500 space-x-reverse space-x1 mr2 [&_span]:text-12px">
+                        <span>ورود</span>
+                        <span>/</span>
+                        <span>ثبت‌ نام</span>
+                    </div>
+                </div>
+                <div :class="imgs.basket" text-2xl text-stone-500 md:text-3xl />
             </div>
 
-            <div grid grid-cols-5 items-center gap-x2 pt2>
+            <div grid grid-cols-5 items-center gap-x2 pt2 md:hidden>
                 <input type="text" placeholder="نام کالا٬ برند یا دسته‌بندی مورد نظر . . ."
-                    class="placeholder:text-8px bg-stone-200/70 pt0.5 pb1.5 px2 rounded-lg col-span-3">
+                    class="text-xs placeholder:text-8px bg-stone-200/70 py2 px2 rounded-lg col-span-3 duration-250 focus:outline-none focus:outline focus:outline-stone-400 sm:placeholder:text-10px">
 
                 <div class="flex items-center border border-stone-100 px2 py-3px rounded-lg col-span-2 justify-center">
                     <div :class="imgs.user" />
-                    <div class="[&_span]:text-10px space-x-reverse space-x1 mr2">
+                    <div class="[&_span]:text-10px space-x-reverse space-x1 mr2 [&_span]:text-stone-500">
                         <span>ورود</span>
                         <span>/</span>
                         <span>ثبت‌ نام</span>
